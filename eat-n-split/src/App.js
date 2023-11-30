604daa296973d39799365af64d01638618a3b0d3
Mohammed Shousha <mshousha29@gmail.com>
2023-11-30 22:41:31 +0200

    Displaying the New Friend Form

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const initialFriends = [
   {
     id: 118836,
@@ -19,13 +21,29 @@ const initialFriends = [
   },
 ];
 
+function Button({ children, onClick }) {
+  return (
+    <button className='button' onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className='app'>
       <div className='sidebar'>
         <FriendsList friends={initialFriends} />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? 'Close' : 'Add Friend'}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
@@ -102,8 +120,4 @@ function FormSplitBill() {
       <Button>Split Bill</Button>
     </form>
   );
-}
-
-function Button({ children }) {
-  return <button className='button'>{children}</button>;
 }
