0d64c026ca73f3d78e153e1d503f19d7a97f98a2
Mohammed Shousha <mshousha29@gmail.com>
2023-12-17 21:37:19 +0200

    Displaying Progress

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -7,6 +7,7 @@ import Error from './Error';
 import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
+import Progress from './Progress';
 
 const initialState = {
   questions: [],
@@ -45,13 +46,16 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
-
+  const maxPossiblePoints = questions.reduce(
+    (acc, question) => acc + question.points,
+    0
+  );
   useEffect(function () {
     fetch('http://localhost:8000/questions')
       .then((res) => res.json())
@@ -71,6 +75,13 @@ export default function App() {
         )}
         {status === 'active' && (
           <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
             <Question
               question={questions[index]}
               dispatch={dispatch}
