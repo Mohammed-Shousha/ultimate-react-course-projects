1b48def94991a91f1bcc16ee48183f11bc46d0e0
Mohammed Shousha <mshousha29@gmail.com>
2023-12-19 19:22:54 +0200

    Building the Pages

--- worldwise/src/pages/Pricing.jsx
+++ worldwise/src/pages/Pricing.jsx
@@ -1,13 +1,27 @@
 import PageNav from '../components/PageNav';
 
-function Pricing() {
-  return (
-    <div>
-      <PageNav />
+// Uses the same styles as Product
+import styles from './Product.module.css';
 
-      <h1>Pricing</h1>
-    </div>
+export default function Pricing() {
+  return (
+    <main className={styles.product}>
+      <PageNav />
+      <section>
+        <div>
+          <h2>
+            Simple pricing.
+            <br />
+            Just $9/month.
+          </h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
+            labore mollitia iusto. Recusandae quos provident, laboriosam fugit
+            voluptatem iste.
+          </p>
+        </div>
+        <img src='img-2.jpg' alt='overview of a large city with skyscrapers' />
+      </section>
+    </main>
   );
 }
-
-export default Pricing;
