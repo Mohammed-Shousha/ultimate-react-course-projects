7cb331955f3dab3f7369fb3d36a81c441054d420
Mohammed Shousha <mshousha29@gmail.com>
2023-12-21 21:12:25 +0200

    Consuming the CitiesContext

--- worldwise/src/contexts/CitiesContext.jsx
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect, useContext } from 'react';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -31,4 +31,14 @@ function CitiesProvider({ children }) {
   );
 }
 
-export { CitiesProvider };
+function useCities() {
+  const context = useContext(CitiesContext);
+
+  if (context === undefined) {
+    throw new Error('useCities must be used within a CitiesProvider');
+  }
+
+  return context;
+}
+
+export { CitiesProvider, useCities };
