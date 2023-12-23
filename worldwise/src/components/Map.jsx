8a3a6e9bf9a0c4c3a16d2cadcf42ba6d7796980f
Mohammed Shousha <mshousha29@gmail.com>
2023-12-23 18:02:03 +0200

    cleaning code and changing title

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import {
   MapContainer,
   TileLayer,
