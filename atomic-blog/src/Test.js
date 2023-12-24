ffb91249014c61350b522bfd2da11429e7445e55
Mohammed Shousha <mshousha29@gmail.com>
2023-12-24 16:42:00 +0200

    A Surprising Optimization Trick With children

--- atomic-blog/src/Test.js
+++ atomic-blog/src/Test.js
@@ -14,13 +14,31 @@ function SlowComponent() {
   );
 }
 
-export default function Test() {
+function Counter({ children }) {
   const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Slow counter?!?</h1>
       <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
-      <SlowComponent />
+      {children}
     </div>
+  );
+}
+
+export default function Test() {
+  // const [count, setCount] = useState(0);
+  // return (
+  //   <div>
+  //     <h1>Slow counter?!?</h1>
+  //     <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+  //     <SlowComponent />
+  //   </div>
+  // );
+
+  /* Using this trick (passing a component as children or as a prop "element") React will no longer re-render the SlowComponent because the SlowComponent this time has been passed in as a children prop, So that means that the SlowComponent was actually created before the Counter component re-rendered and there is no way in which the SlowComponent could have been affected by the state change in the Counter component */
+  return (
+    <Counter>
+      <SlowComponent />
+    </Counter>
   );
 }
