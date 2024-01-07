60af15000fb6b188688e5c660e88874db4fa5db1
Mohammed Shousha <mshousha29@gmail.com>
2024-01-07 16:45:11 +0200

    Fetching Cabin Data

--- the-wild-oasis/src/pages/Cabins.jsx
+++ the-wild-oasis/src/pages/Cabins.jsx
@@ -1,19 +1,20 @@
-import { useEffect } from 'react';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import { getCabins } from '../services/apiCabins';
+
+import CabinTable from '../features/cabins/CabinTable';
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((cabins) => console.log(cabins));
-  }, []);
-
   return (
-    <Row type='horizontal'>
-      <Heading as='h1'>All cabins</Heading>
-      <p>TEST</p>
-      <img src='https://nchebwndndhfsofpbqvi.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg' />
-    </Row>
+    <>
+      <Row type='horizontal'>
+        <Heading as='h1'>All cabins</Heading>
+        <p>Fliter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
