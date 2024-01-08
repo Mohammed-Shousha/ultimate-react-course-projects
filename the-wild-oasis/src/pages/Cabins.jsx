a9dec9dbd6fecaf0f8a4b327d82d6b5e0127a90f
Mohammed Shousha <mshousha29@gmail.com>
2024-01-08 22:21:52 +0200

    React Hook Form

--- the-wild-oasis/src/pages/Cabins.jsx
+++ the-wild-oasis/src/pages/Cabins.jsx
@@ -1,9 +1,15 @@
-import Heading from '../ui/Heading';
-import Row from '../ui/Row';
+import { useState } from 'react';
 
 import CabinTable from '../features/cabins/CabinTable';
+import CreateCabinForm from '../features/cabins/CreateCabinForm';
+
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+import Button from '../ui/Button';
 
 function Cabins() {
+  const [showFrom, setShowForm] = useState(false);
+
   return (
     <>
       <Row type='horizontal'>
@@ -13,6 +19,10 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showFrom && <CreateCabinForm />}
       </Row>
     </>
   );
