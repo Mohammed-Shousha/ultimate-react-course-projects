ad0e64b23b2e6b808fe019218d4b526bbe3ce09a
Mohammed Shousha <mshousha29@gmail.com>
2024-02-25 22:08:02 +0200

    API-Side Sorting: Sorting Bookings

--- the-wild-oasis/src/features/bookings/useBookings.js
+++ the-wild-oasis/src/features/bookings/useBookings.js
@@ -13,13 +13,20 @@ export function useBookings() {
       : { field: 'status', value: filterValue };
   // :{ field: 'totalPrice', value: 5000, method: 'gte' };
 
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, order] = sortByRaw.split('-');
+  const sortBy = {
+    field,
+    order,
+  };
+
   const {
     data: bookings,
     isLoading,
     error,
   } = useQuery({
-    queryKey: ['bookings', filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { bookings, isLoading, error };
