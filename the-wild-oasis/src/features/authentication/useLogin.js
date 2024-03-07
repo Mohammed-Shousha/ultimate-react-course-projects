c413bec517c80ccaa58e3bb17375042ff0447028
Mohammed Shousha <mshousha29@gmail.com>
2024-03-07 17:49:51 +0200

    Authorization: Protecting Routes

--- the-wild-oasis/src/features/authentication/useLogin.js
+++ the-wild-oasis/src/features/authentication/useLogin.js
@@ -1,14 +1,17 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { login as loginApi } from '../../services/apiAuth';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 
 export function useLogin() {
+  const queryClient = useQueryClient();
+
   const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: loginApi,
-    onSuccess: () => {
+    onSuccess: (data) => {
+      queryClient.setQueriesData(['user'], data?.user);
       navigate('/dashboard');
     },
     onError: (error) => {
