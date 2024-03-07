5ab2684dd56060b6c4c332d8f2b42b9dcfa03bae
Mohammed Shousha <mshousha29@gmail.com>
2024-03-07 17:26:52 +0200

    Authentication: User Login With Supabase

---
+++ the-wild-oasis/src/features/authentication/useLogin.js
@@ -0,0 +1,21 @@
+import { useMutation } from '@tanstack/react-query';
+import { login as loginApi } from '../../services/apiAuth';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-hot-toast';
+
+export function useLogin() {
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: loginApi,
+    onSuccess: () => {
+      navigate('/dashboard');
+    },
+    onError: (error) => {
+      console.error('loginError', error);
+      toast.error('Provided credentials are invalid');
+    },
+  });
+
+  return { login, isLoading };
+}
