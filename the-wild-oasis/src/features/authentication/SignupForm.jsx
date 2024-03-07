21bda8e0f7d9b6cb576504380bbc9c849be43257
Mohammed Shousha <mshousha29@gmail.com>
2024-03-07 19:23:38 +0200

    User Sign Up

--- the-wild-oasis/src/features/authentication/SignupForm.jsx
+++ the-wild-oasis/src/features/authentication/SignupForm.jsx
@@ -4,15 +4,22 @@ import Button from '../../ui/Button';
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
-
-// Email regex: /\S+@\S+\.\S+/
+import { useSignUp } from './useSignUp';
 
 function SignupForm() {
-  const { register, formState, getValues, handleSubmit } = useForm();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
   const { errors } = formState;
 
-  function onSubmit(data) {
-    console.log(data);
+  const { signUp, isLoading } = useSignUp();
+  function onSubmit({ fullName, email, password }) {
+    signUp(
+      { fullName, email, password },
+      {
+        onSettled: () => {
+          reset();
+        },
+      }
+    );
   }
 
   return (
@@ -21,6 +28,7 @@ function SignupForm() {
         <Input
           type='text'
           id='fullName'
+          disabled={isLoading}
           {...register('fullName', { required: 'This field is required' })}
         />
       </FormRow>
@@ -29,6 +37,7 @@ function SignupForm() {
         <Input
           type='email'
           id='email'
+          disabled={isLoading}
           {...register('email', {
             required: 'This field is required',
             pattern: {
@@ -46,6 +55,7 @@ function SignupForm() {
         <Input
           type='password'
           id='password'
+          disabled={isLoading}
           {...register('password', {
             required: 'This field is required',
             minLength: {
@@ -60,6 +70,7 @@ function SignupForm() {
         <Input
           type='password'
           id='passwordConfirm'
+          disabled={isLoading}
           {...register('passwordConfirm', {
             required: 'This field is required',
             validate: (value) =>
@@ -70,10 +81,10 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation='secondary' type='reset'>
+        <Button variation='secondary' type='reset' disabled={isLoading}>
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}> Create new user</Button>
       </FormRow>
     </Form>
   );
