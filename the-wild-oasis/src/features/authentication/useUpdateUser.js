caa9be431c69215cd8fbfd34e0c639c0141f7cd2
Mohammed Shousha <mshousha29@gmail.com>
2024-03-31 04:20:40 +0200

    Implementing Dark Mode With CSS Variables

--- the-wild-oasis/src/features/authentication/useUpdateUser.js
+++ the-wild-oasis/src/features/authentication/useUpdateUser.js
@@ -8,9 +8,9 @@ export function useUpdateUser() {
 
   const { mutate: updateUser, isLoading: isUpdating } = useMutation({
     mutationFn: updateCurrentUser,
-    onSuccess: () => {
+    onSuccess: ({ user }) => {
       toast.success('User updated successfully');
-      queryClient.invalidateQueries(['user']);
+      queryClient.setQueryData(['user'], user);
     },
     onError: (err) => toast.error(err.message),
   });
--- the-wild-oasis/src/ui/HeaderMenu.jsx
+++ the-wild-oasis/src/ui/HeaderMenu.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import ButtonIcon from './ButtonIcon';
 import { HiOutlineUser } from 'react-icons/hi2';
 import Logout from '../features/authentication/Logout';
+import DarkModeToggle from './DarkModeToggle';
 
 const StyledHeaderMenu = styled.ul`
   display: flex;
@@ -23,6 +24,9 @@ function HeaderMenu() {
         >
           <HiOutlineUser />
         </ButtonIcon>
+      </li>
+      <li>
+        <DarkModeToggle />
       </li>
       <li>
         <Logout />
