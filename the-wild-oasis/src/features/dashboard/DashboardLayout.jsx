09ad3ab2bd4d72a1cf050a1e781d99b7a25d4906
Mohammed Shousha <mshousha29@gmail.com>
2024-04-12 21:28:23 +0200

    Displaying a Line Chart With the Recharts Library

--- the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import { useCabins } from '../cabins/useCabins';
 
 import Spinner from '../../ui/Spinner';
 import Stats from './Stats';
+import SalesChart from './SalesChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -38,7 +39,7 @@ function DashboardLayout() {
       />
       <div>Today's Activity</div>
       <div>Chart stay durations</div>
-      <div>Chart sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
