861d0bd948c4b106da4311350ef7c9fbdde98f4c
Mohammed Shousha <mshousha29@gmail.com>
2024-07-27 05:48:12 +0300

    Creating a Layout

--- the-wild-oasis-website/app/page.js
+++ the-wild-oasis-website/app/page.js
@@ -1,10 +1,3 @@
-import Navigation from "./components/Navigation";
-
 export default function Page() {
-  return (
-    <>
-      <Navigation />
-      <h1>Welcome to The Wild Oasis</h1>
-    </>
-  );
+  return <h1>Welcome to The Wild Oasis</h1>;
 }
