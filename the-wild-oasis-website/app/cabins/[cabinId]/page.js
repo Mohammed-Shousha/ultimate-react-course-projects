b6aa1932718f2a58ac10d99e98786c0331d83e05
Mohammed Shousha <mshousha29@gmail.com>
2024-07-30 05:19:37 +0300

    Making Dynamic Pages Static With generateStaticParams

--- the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,6 +1,6 @@
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 
 import Image from "next/image";
 
@@ -10,6 +10,14 @@ export async function generateMetadata({ params }) {
   return {
     title: `Cabin ${name}`,
   };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map(({ id }) => ({ cabinId: String(id) }));
+
+  return ids;
 }
 
 export default async function Page({ params }) {
