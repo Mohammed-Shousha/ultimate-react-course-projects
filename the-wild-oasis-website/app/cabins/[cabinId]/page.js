eadc3ac02b975a84e10061e9ea2e58f2876576db
Mohammed Shousha <mshousha29@gmail.com>
2024-08-11 04:23:38 +0300

    Client Components in Server Components

--- the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,8 +1,9 @@
-import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
+import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
 
-import { getCabin, getCabins } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from '@/app/_lib/data-service';
 
-import Image from "next/image";
+import Image from 'next/image';
+import TextExpander from '@/app/_components/TextExpander';
 
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
@@ -43,20 +44,22 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
               <UsersIcon className="h-5 w-5 text-primary-600" />
               <span className="text-lg">
-                For up to <span className="font-bold">{maxCapacity}</span>{" "}
+                For up to <span className="font-bold">{maxCapacity}</span>{' '}
                 guests
               </span>
             </li>
             <li className="flex gap-3 items-center">
               <MapPinIcon className="h-5 w-5 text-primary-600" />
               <span className="text-lg">
-                Located in the heart of the{" "}
+                Located in the heart of the{' '}
                 <span className="font-bold">Dolomites</span> (Italy)
               </span>
             </li>
