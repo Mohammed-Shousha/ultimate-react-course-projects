2b6381d091ecca6738c4d4867dcb9fa258830b6e
Mohammed Shousha <mshousha29@gmail.com>
2024-08-12 22:41:05 +0300

    Updating the Profile Using a Server Action

--- the-wild-oasis-website/app/_lib/actions.js
+++ the-wild-oasis-website/app/_lib/actions.js
@@ -1,6 +1,38 @@
 "use server";
 
-import { signIn, signOut } from "@/app/_lib/auth";
+import { supabase } from "@/app/_lib/supabase";
+import { signIn, signOut, auth } from "@/app/_lib/auth";
+
+export async function updateGuest(formData) {
+  const session = await auth();
+
+  if (!session) {
+    throw new Error("You need to be signed in to update your profile.");
+  }
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid national ID");
+
+  const updateData = {
+    nationalID,
+    nationality,
+    countryFlag,
+  };
+
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session?.user?.guestId)
+    .select()
+    .single();
+
+  if (error) throw new Error("Guest could not be updated");
+
+  console.log(data);
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
