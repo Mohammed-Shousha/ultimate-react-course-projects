c1330a428b58633932df30c6103a1812e477315e
Mohammed Shousha <mshousha29@gmail.com>
2024-08-14 18:14:38 +0300

    Defining Page Title and Favicon

--- nextjs-pages-router/pages/cabins/[cabinId].js
+++ nextjs-pages-router/pages/cabins/[cabinId].js
@@ -1,8 +1,20 @@
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 function Cabin() {
   const router = useRouter();
-  return <div>Cabin #{router.query.cabinId}</div>;
+
+  const { cabinId } = router.query;
+
+  return (
+    <>
+      <Head>
+        <title>Cabin {cabinId} | The Wild Oasis</title>
+      </Head>
+
+      <div>Cabin #{cabinId}</div>
+    </>
+  );
 }
 
 export default Cabin;
